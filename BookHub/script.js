ffa4b59c858e620034ce2402b7b447ace182569c
saravanan-013 <126344463+saravanan-013@users.noreply.github.com>
2024-03-21 12:09:07 +0530

    Add files via upload

---
+++ BookHub/script.js
@@ -0,0 +1,51 @@
+
+var popupoverlay=document.querySelector(".popup-overlay")
+var popupbox=document.querySelector(".popup-box")
+var addbutton=document.getElementById("add-popup-button")
+
+addbutton.addEventListener("click",function(){
+
+    popupoverlay.style.display="block"
+    popupbox.style.display="block"
+})
+
+
+var cancelpopup=document.getElementById("cancel-popup")
+ cancelpopup.addEventListener("click",function(event){
+    event.preventDefault()
+    popupoverlay.style.display="none"
+    popupbox.style.display="none"
+ })
+
+var addbook=document.getElementById("add-book")
+var container=document.querySelector(".container")
+var booktitleinput=document.getElementById("book-title-input")
+var bookauthorinput=document.getElementById("book-author-input")
+var bookdescriptioninput=document.getElementById("book-description-input")
+
+addbook.addEventListener("click",function(event){
+    event.preventDefault()
+    var div=document.createElement("div")
+    div.setAttribute("class","book-container")
+    div.innerHTML=`<h2>${booktitleinput.value}</h2>
+        <h5>${bookauthorinput.value}</h5>
+        <p>${bookdescriptioninput.value}</p>
+        <button onclick="deletebook(event)">Delete</button>`
+    container.append(div)
+
+booktitleinput.value = "";
+bookauthorinput.value = "";
+bookdescriptioninput.value = "";
+
+
+popupoverlay.style.display = "none";
+popupbox.style.display = "none";
+
+ 
+
+})
+
+function deletebook(event)
+{
+event.target.parentElement.remove()
+}
